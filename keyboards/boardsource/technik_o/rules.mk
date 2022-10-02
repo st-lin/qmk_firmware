cb4f31fbc4c1a67314567f90f5458e0d30b8985c
Stefan Lindholm <stefan.lindholm@sectra.se>
2022-10-02 23:03:43 +0200

    Multilayer technik

--- keyboards/boardsource/technik_o/rules.mk
+++ keyboards/boardsource/technik_o/rules.mk
@@ -1,2 +1,13 @@
+# MCU name
+MCU = atmega32u4
+
+# Bootloader selection
+BOOTLOADER = atmel-dfu
+
 RGB_MATRIX_DRIVER = WS2812
+
 COMBO_ENABLE = yes
+KEY_OVERRIDE_ENABLE = yes
+SEND_STRING_ENABLE = yes
+
+EXTRAFLAGS += -flto
