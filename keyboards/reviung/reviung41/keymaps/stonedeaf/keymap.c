3cebba934fdda7f6fa2e629389dccde8bc145aec
Stefan Lindholm <stefan.lindholm@sectra.se>
2022-10-05 07:12:42 +0200

    [Reviung] Num <-> Sym

--- keyboards/reviung/reviung41/keymaps/stonedeaf/keymap.c
+++ keyboards/reviung/reviung41/keymaps/stonedeaf/keymap.c
@@ -53,15 +53,13 @@ enum custom_keycodes {
 #define OS_RSFT OSM(MOD_RSFT)
 
 #define L_MID OS_LSFT//KC_LSPO   //  (  or SHIFT
-#define R_MID OS_RSFT//KC_RSPC   //  )  or SHIFT
+#define R_MID RSFT_T(KC_ENT) // OS_RSFT//KC_RSPC   //  )  or SHIFT
 #define L_BOT KC_LCPO   // DEL or CTL
 #define R_BOT LALT_T(KC_ENT) // ENT or ALT
 
-#define SPC_SYM LT(_SYM, KC_SPC)
-#define ENT_NUM LT(_NUM, KC_ENT)
-#define NUM TT(_NUM)
-#define NAV TT(_NAV)
-#define SYM MO(_SYM)
+#define NUM_SPC LT(_NUM, KC_SPC)
+#define SYM_ENT LT(_SYM, KC_ENT)
+#define NAV MO(_NAV)
  
 #define TOP_LAYER_NAV_CLUSTER
 
@@ -79,36 +77,36 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,               KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
     L_MID,    KC_A,     KC_S,     KC_D,     KC_F,     KC_G,               KC_H,     KC_J,     KC_K,     KC_L,     KC_UP,    R_MID,
     L_BOT,    KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,               KC_N,     KC_M,     ___N___,  KC_LEFT,  KC_DOWN,  KC_RGHT,
-                                            GUI_ALT,  SPC_SYM, TG(_SWE),  ENT_NUM,  MO(_NAV)
+                                            GUI_ALT,  NUM_SPC, TG(_SWE),  SYM_ENT,  NAV
   ),
 #endif
   [_SWE] = LAYOUT_reviung41(
     _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,  _______,  _______,  _______,  SW_AA,
     _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,  _______,  _______,  SW_OE,    SW_AE,
     _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,  K_COMM,   K_DOT,    K_MINS,   KC_ENT,
-                                            _______,  _______, TG(_SWE),  _______,  _______
+                                            _______,  KC_SPC,  TG(_SWE),  KC_ENT,   _______
   ),
 
   [_NUM] = LAYOUT_reviung41(
-    _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,  S(KC_F12),            KC_PSLS,  KC_P7,    KC_P8,    KC_P9,    KC_PMNS,  _______,
-    _______,  KC_F5,    KC_F6,    KC_F7,    KC_F8,  C(KC_F12),            KC_PAST,  KC_P4,    KC_P5,    KC_P6,    KC_PPLS,  _______,
-    _______,  KC_F9,    KC_F10,   KC_F11,   KC_F12, A(KC_HOME),           KC_KP_0,  KC_P1,    KC_P2,    KC_P3,    KC_PDOT,  KC_PENT,
-                                            _______,  KC_SPC,  TG(_NUM),  TG(_NUM),  ___N___
+    K_QUES,   K_EXLM,   K_AT,     K_HASH,   K_DLR,    K_PERC,             KC_P7,    KC_P8,    KC_P9,    ___N___,  ___N___,  _______,
+    KC_0,     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,               KC_P4,    KC_P5,    KC_P6,    ___N___,  ___N___,  _______,
+    _______,  C(KC_Z),  C(KC_X),  C(KC_C),  C(KC_V),  ___N___,            KC_P1,    KC_P2,    KC_P3,    ___N___,  ___N___,  KC_ENT,
+                                            _______,  KC_SPC,   ___N___,  KC_0,     KC_PDOT
   ),
 
   [_NAV] = LAYOUT_reviung41(
-    KC_0,     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,               ___N___,  ___N___,  ___N___,  ___N___,  ___N___,  _______,
-    _______,  KC_6,     KC_7,     KC_8,     KC_9,     KC_0,               ___N___,  ___N___,  KC_PGUP,  ___N___,  KC_HOME,  _______,
-    _______,  KC_LGUI,  KC_LALT,  KC_LCTL,  KC_LSFT,  KC_DEL,             ___N___,  ___N___,  KC_PGDN,  C_LEFT,   KC_END,   C_RGHT,
-                                            _______,  KC_SPC,  TG(_NAV),  TG(_NAV), ___N___
+    KC_0,     KC_F1,    KC_F2,    KC_F3,    KC_F4,  S(KC_F12),            ___N___,  ___N___,  ___N___,  ___N___,  ___N___,  _______,
+    _______,  KC_F5,    KC_F6,    KC_F7,    KC_F8,  C(KC_F12),            ___N___,  ___N___,  KC_PGUP,  ___N___,  KC_HOME,  _______,
+    _______,  KC_F9,    KC_F10,   KC_F11,   KC_F12, A(KC_HOME),           ___N___,  ___N___,  KC_PGDN,  C_LEFT,   KC_END,   C_RGHT,
+                                            _______,  KC_SPC,   ___N___,  KC_ENT,   ___N___
   ),
 
 #ifdef TOP_LAYER_NAV_CLUSTER
   [_SYM] = LAYOUT_reviung41(
-    K_QUES,   K_EXLM,   K_AT,     K_HASH,   K_DLR,    K_PERC,             ___N___,  ___N___,  ___N___,  ___N___,  ___N___,  _______,
-    _______,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,               K_PIPE,  K_AMPR,    ___N___,  ___N___,  MY_SCLN,  _______,
-    _______,  C(KC_Z),  C(KC_X),  C(KC_C),  C(KC_V),  ___N___,            K_CIRC,  K_TILD,    K_COMM,   K_DOT,    K_MINS,   KC_ENT,
-                                            _______,  KC_SPC,  TG(_SYM),  ___N___,  ___N___
+    _______,  ___N___,  ___N___,  ___N___,  ___N___,  ___N___,            ___N___,  ___N___,  ___N___,  ___N___,  ___N___,  _______,
+    _______,  ___N___,  ___N___,  ___N___,  ___N___,  ___N___,            ___N___,  ___N___,  ___N___,  ___N___,  MY_SCLN,  _______,
+    _______,  ___N___,  ___N___,  ___N___,  ___N___,  ___N___,            ___N___,  ___N___,  K_COMM,   K_DOT,    K_MINS,   KC_ENT,
+                                            _______,  KC_SPC,   ___N___,  KC_ENT,   ___N___
   ),
 #else
   [_SYM] = LAYOUT_reviung41(
@@ -352,7 +350,7 @@ const rgblight_segment_t PROGMEM rgb_num[] = RGBLIGHT_LAYER_SEGMENTS(
     {10, 1, HSV_BLUE}
 );
 const rgblight_segment_t PROGMEM rgb_nav[] = RGBLIGHT_LAYER_SEGMENTS(
-    {10, 1, HSV_RED}
+    {10, 1, HSV_CYAN}
 );
 const rgblight_segment_t PROGMEM rgb_sym[] = RGBLIGHT_LAYER_SEGMENTS(
     {10, 1, HSV_GREEN}
@@ -383,7 +381,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 
 void oneshot_mods_changed_user(uint8_t mods) {
   if (mods & MOD_MASK_SHIFT) {
-    rgblight_sethsv_at(HSV_MAGENTA, 10);
+    rgblight_sethsv_at(HSV_RED, 10);
   }
   if (!mods) {
     default_layer_state_set_user(layer_state);
@@ -392,7 +390,7 @@ void oneshot_mods_changed_user(uint8_t mods) {
 }
 void oneshot_locked_mods_changed_user(uint8_t mods) {
   if (mods & MOD_MASK_SHIFT) {
-    rgblight_sethsv_at(HSV_MAGENTA, 10);
+    rgblight_sethsv_at(HSV_RED, 10);
   }
   if (!mods) {
     default_layer_state_set_user(layer_state);
