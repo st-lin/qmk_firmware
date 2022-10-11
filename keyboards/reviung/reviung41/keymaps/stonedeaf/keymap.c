ad991749a5df83d17a67d0aa14585909ede28ddb
Stefan Lindholm <stefan.lindholm@sectra.se>
2022-10-11 18:10:38 +0200

    [Reviung] Removed top-nav

--- keyboards/reviung/reviung41/keymaps/stonedeaf/keymap.c
+++ keyboards/reviung/reviung41/keymaps/stonedeaf/keymap.c
@@ -28,6 +28,7 @@ enum layer_names {
 };
 
 #define ___N___ KC_NO
+#define __XXX__ KC_NO
 
 enum custom_keycodes {
   GUI_ALT = SAFE_RANGE,
@@ -69,78 +70,51 @@ enum custom_keycodes {
 #define SYM_ENT LT(_SYM, KC_ENT)
 #define NAV MO(_NAV)
 #define EXT MO(_EXT)
- 
-//#define TOP_LAYER_NAV_CLUSTER
+
 #define LEFT_LAYER_NAV_CLUSTER
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-#ifndef TOP_LAYER_NAV_CLUSTER
   [_BASE] = LAYOUT_reviung41(
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,               KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
     L_MID,    KC_A,     KC_S,     KC_D,     KC_F,     KC_G,               KC_H,     KC_J,     KC_K,     KC_L,     MY_SCLN,  R_MID,
     L_BOT,    KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,               KC_N,     KC_M,     KC_COMM,  KC_DOT,   K_MINS,   R_BOT,
                                             EXT,      NUM_SPC, TG(_SWE),  SYM_ENT,  NAV
   ),
-#else
-  [_BASE] = LAYOUT_reviung41(
-    KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,               KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
-    L_MID,    KC_A,     KC_S,     KC_D,     KC_F,     KC_G,               KC_H,     KC_J,     KC_K,     KC_L,     KC_UP,    R_MID,
-    L_BOT,    KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,               KC_N,     KC_M,     ___N___,  KC_LEFT,  KC_DOWN,  KC_RGHT,
-                                            GUI_ALT,  NUM_SPC, TG(_SWE),  SYM_ENT,  NAV
-  ),
-#endif
+
   [_SWE] = LAYOUT_reviung41(
     _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,  _______,  _______,  _______,  SW_AA,
     _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,  _______,  _______,  SW_OE,    SW_AE,
     _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,  K_COMM,   K_DOT,    K_MINS,   KC_ENT,
-                                            _______,  KC_SPC,  TG(_SWE),  KC_ENT,   _______
+                                            _______,  _______, TG(_SWE),  _______,  _______
   ),
 
   [_NUM] = LAYOUT_reviung41(
     K_QUES,   K_EXLM,   K_AT,     K_HASH,   K_DLR,    K_PERC,             KC_P7,    KC_P8,    KC_P9,    ___N___,  ___N___,  _______,
     KC_0,     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,               KC_P4,    KC_P5,    KC_P6,    ___N___,  ___N___,  _______,
     _______,  C(KC_Z),  C(KC_X),  C(KC_C),  C(KC_V),  ___N___,            KC_P1,    KC_P2,    KC_P3,    ___N___,  ___N___,  KC_ENT,
-                                            ___N___,  ___N___,  ___N___,  KC_0,     KC_PDOT
+                                            ___N___,  __XXX__,  ___N___,  KC_0,     KC_PDOT
   ),
 
-#ifdef TOP_LAYER_NAV_CLUSTER
-  [_NAV] = LAYOUT_reviung41(
-    _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    ___N___,            KC_F5,    KC_F6,    KC_F10,   KC_F11,   KC_F12,   _______,
-    _______,  KC_F5,    KC_F6,    KC_F7,    KC_F8,    ___N___,            ___N___,  ___N___,  KC_PGUP,  ___N___,  KC_HOME,  _______,
-    _______,  KC_F9,    KC_F10,   KC_F11,   KC_F12,   ___N___,            ___N___,  ___N___,  KC_PGDN,  C_LEFT,   KC_END,   C_RGHT,
-                                            KC_LALT,  KC_SPC,   ___N___,  KC_ENT,   ___N___
-  ),
-#else
   [_NAV] = LAYOUT_reviung41(
     _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,              KC_F5,    KC_F6,    KC_F10,   KC_F11,   KC_F12,   _______,
     _______,  KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,             ___N___,  ___N___,  KC_PGUP,  KC_HOME,  KC_UP,    KC_END,
     _______,  KC_Z,     KC_X,     KC_C,     KC_V,     ___N___,            ___N___,  ___N___,  KC_PGDN,  KC_LEFT,  KC_DOWN,  KC_RGHT,
-                                            KC_LALT,  KC_SPC,   ___N___,  KC_ENT,   ___N___
+                                            KC_LALT,  KC_SPC,   ___N___,  KC_ENT,   __XXX__
   ),
-#endif
 
-#ifdef TOP_LAYER_NAV_CLUSTER
-  [_SYM] = LAYOUT_reviung41(
-    _______,  ___N___,  ___N___,  ___N___,  ___N___,  ___N___,            ___N___,  ___N___,  ___N___,  ___N___,  ___N___,  _______,
-    _______,  KC_LGUI,  KC_LALT,  KC_LCTL,  KC_LSFT,  ___N___,            ___N___,  ___N___,  ___N___,  ___N___,  MY_SCLN,  _______,
-    _______,  ___N___,  ___N___,  ___N___,  ___N___,  ___N___,            ___N___,  ___N___,  K_COMM,   K_DOT,    K_MINS,   ___N___,
-                                            _______,  KC_SPC,   ___N___,  ___N___,   ___N___
-  ),
-#else
   [_SYM] = LAYOUT_reviung41(
     KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,              KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,
     ___N___,  OS_LGUI,  OS_LALT,  OS_LCTL,  OS_LSFT,  ___N___,            ___N___,  KC_LSFT,  KC_LCTL,  KC_LALT,  KC_LGUI,  ___N___,
     ___N___,  ___N___,  ___N___,  ___N___,  ___N___,  ___N___,            ___N___,  ___N___,  ___N___,  ___N___,  ___N___,  ___N___,
-                                            ___N___,  KC_SPC,   ___N___,  ___N___,  ___N___
+                                            ___N___,  KC_SPC,   ___N___,  __XXX__,  ___N___
   ),
-#endif
 
   [_EXT] = LAYOUT_reviung41(
     ALT_TAB,  ___N___,  G(KC_2),  G(KC_E),  ___N___,  ___N___,            ___N___,  ___N___,  ___N___,  ___N___,  KC_HOME,  KC_END,
     _______,  OS_LGUI,  OS_LALT,  OS_LCTL,  OS_LSFT,  ___N___,            ___N___,  ___N___,  KC_PGUP,  KC_HOME,  KC_UP,    KC_END,
     _______,  C(KC_Z),  C(KC_X),  C(KC_C),  C(KC_V),  ___N___,            ___N___,  ___N___,  KC_PGDN,  KC_LEFT,  KC_DOWN,  KC_RGHT,
-                                            ___N___,  KC_SPC,   ___N___,  KC_ENT,   KC_RCTL
+                                            __XXX__,  KC_SPC,   ___N___,  KC_ENT,   KC_RCTL
   ),
 
   [_COM] = LAYOUT_reviung41(
