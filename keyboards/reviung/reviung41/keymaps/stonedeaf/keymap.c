709411a4df2dbb7b491836e862794ce84efc0445
Stefan Lindholm <stefan.lindholm@sectra.com>
2022-10-14 16:03:42 +0200

    [Reviung] Minors.

--- keyboards/reviung/reviung41/keymaps/stonedeaf/keymap.c
+++ keyboards/reviung/reviung41/keymaps/stonedeaf/keymap.c
@@ -80,28 +80,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_reviung41(
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,               KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
     L_MID,    KC_A,     KC_S,     KC_D,     KC_F,     KC_G,               KC_H,     KC_J,     KC_K,     KC_L,     MY_SCLN,  R_MID,
-    L_BOT,    KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,               KC_N,     KC_M,     KC_COMM,  KC_DOT,   K_MINS,   R_BOT,
+    L_BOT,    KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,               KC_N,     KC_M,     K_COMM,   K_DOT,    K_MINS,   R_BOT,
                                             NAV,      NUM_SPC,  FUN_SPC,  SYM_ENT,  EXT
   ),
 
   [_SWE] = LAYOUT_reviung41(
     _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,  _______,  _______,  _______,  SW_AA,
     _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,  _______,  _______,  SW_OE,    SW_AE,
-    _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,  K_COMM,   K_DOT,    K_MINS,   KC_ENT,
+    _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,  _______,  _______,  _______,  _______,
                                             _______,  _______, TG(_SWE),  _______,  _______
   ),
 
   [_FUN] = LAYOUT_reviung41(
-    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,              KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,  KC_F12,
-    KC_LSFT,  KC_LGUI,  KC_LALT,  KC_LCTL,  KC_LSFT,  ___N___,            ___N___,  ___N___,  ___N___,  ___N___,  ___N___,  ___N___,
-    ___N___,  ___N___,  ___N___,  ___N___,  ___N___,  ___N___,            ___N___,  ___N___,  ___N___,  ___N___,  ___N___,  ___N___,
+    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,              KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,
+    _______,  KC_LGUI,  KC_LALT,  KC_LCTL,  KC_LSFT,  ___N___,            ___N___,  ___N___,  ___N___,  ___N___,  ___N___,  _______,
+    _______,  ___N___,  ___N___,  ___N___,  ___N___,  ___N___,            ___N___,  ___N___,  ___N___,  ___N___,  ___N___,  _______,
                                             ___N___,  ___N___,  __XXX__,  ___N___,  ___N___
   ),
 
   [_NUM] = LAYOUT_reviung41(
-    K_QUES,   K_EXLM,   K_AT,     K_HASH,   K_DLR,    K_PERC,             KC_P7,    KC_P8,    KC_P9,    ___N___,  ___N___,  SW_AA,
-    KC_0,     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,               KC_P4,    KC_P5,    KC_P6,    ___N___,  SW_OE,    SW_AE,
-    _______,  C(KC_Z),  C(KC_X),  C(KC_C),  C(KC_V),  ___N___,            KC_P1,    KC_P2,    KC_P3,    ___N___,  ___N___,  KC_ENT,
+    K_QUES,   K_EXLM,   K_AT,     K_HASH,   K_DLR,    K_PERC,             KC_P7,    KC_P8,    KC_P9,    KC_PMNS,  KC_PSCR,  SW_AA,
+    KC_0,     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,               KC_P4,    KC_P5,    KC_P6,    KC_PPLS,  SW_OE,    SW_AE,
+    _______,  C(KC_Z),  C(KC_X),  C(KC_C),  C(KC_V),  ___N___,            KC_P1,    KC_P2,    KC_P3,    _______,  _______,  _______,
                                             ___N___,  __XXX__,  ___N___,  KC_0,     KC_PDOT
   ),
 
