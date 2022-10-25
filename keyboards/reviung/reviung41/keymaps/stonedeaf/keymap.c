d79b28e742e9803d6a5cea3a5d2f47dec7db4eb7
Stefan Lindholm <stefan.lindholm@sectra.com>
2022-10-25 07:12:38 +0200

    [Reviung] Fixes.

--- keyboards/reviung/reviung41/keymaps/stonedeaf/keymap.c
+++ keyboards/reviung/reviung41/keymaps/stonedeaf/keymap.c
@@ -98,33 +98,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_FUN] = LAYOUT_reviung41(
     _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,              KC_F6,    KC_F7,    KC_VOLU,  KC_CALC,  KC_PSCR,  _______,
     _______,  KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,             KC_HOME,  ___N___,  KC_VOLD,  ___N___,  ___N___,  KC_RSFT,
-    _______,  ___N___,  ___N___,  ___N___,  ___N___,  KC_DEL,             KC_END,   ___N___,  KC_MUTE,  ___N___,  KC_LALT,  KC_RCTL,
+    _______,  ___N___,  ___N___,  ___N___,  ___N___,  KC_DEL,             KC_END,   ___N___,  KC_MPLY,  ___N___,  KC_LALT,  KC_RCTL,
                                             KC_WBAK,  VS_BACK, TT(_FUN),  VS_FWD,  KC_WFWD
   ),
 
   [_NUM] = LAYOUT_reviung41(
     KC_0,     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,               KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     SW_AA,
-    K_TILD,   K_EXLM,   K_AT,     K_HASH,   K_DLR,    K_PERC,             ___N___,  ___N___,  ___N___,  ___N___,  SW_OE,    SW_AE,
-    _______,  C(KC_Z),  C(KC_X),  C(KC_C),  C(KC_V),  ___N___,            ___N___,  ___N___,  ___N___,  ___N___,  ___N___,  _______,
-                                            ___N___,  __XXX__,  ___N___,  ___N___,  ___N___
+    K_TILD,   K_EXLM,   K_AT,     K_HASH,   K_DLR,    K_PERC,             ___N___,  KC_4,     KC_5,     KC_6,     SW_OE,    SW_AE,
+    _______,  C(KC_Z),  C(KC_X),  C(KC_C),  C(KC_V),  ___N___,            KC_0,     KC_1,     KC_2,     KC_3,     ___N___,  _______,
+                                            ___N___,  __XXX__,  ___N___,  KC_0,     KC_DOT
   ),
 
   [_NAV] = LAYOUT_reviung41(
     ALT_TAB,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,               KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     _______,
-    _______,  K_EXLM,   K_AT,     K_HASH,   K_DLR,    K_PERC,             ___N___,  ___N___,  KC_PGUP,  KC_HOME,  KC_UP,    KC_END,
+    _______,  K_EXLM,   K_AT,     K_HASH,   K_DLR,    K_PERC,             K_TILD,   K_CIRC,   KC_PGUP,  KC_HOME,  KC_UP,    KC_END,
     _______,  C(KC_Z),  C(KC_X),  C(KC_C),  C(KC_V),  ___N___,            ___N___,  ___N___,  KC_PGDN,  KC_LEFT,  KC_DOWN,  KC_RGHT,
                                             __XXX__,  KC_SPC,   ___N___,  KC_ENT,   KC_RCTL    
   ),
 
   [_SYM] = LAYOUT_reviung41(
-    _______,  ___N___,  ___N___,  ___N___,  K_PIPE,   K_LBRC,             K_RBRC,   K_AMPR,   KC_P7,    KC_P8,    KC_P9,    KC_PMNS,
-    _______,  ___N___,  ___N___,  ___N___,  K_LPRN,   K_LCBR,             K_RCBR,   K_RPRN,   KC_P4,    KC_P5,    KC_P6,    KC_PPLS,
-    _______,  ___N___,  ___N___,  ___N___,  K_SLSH,   K_LT,               K_GT,     K_BSLS,   KC_P1,    KC_P2,    KC_P3,    KC_P0,
-                                            ___N___,  KC_SPC,   ___N___,  __XXX__,  ___N___
+    _______,  K_EQUAL,  ___N___,  ___N___,  K_PIPE,   K_LBRC,             K_RBRC,   K_AMPR,   KC_P7,    KC_P8,    KC_P9,    _______,
+    _______,  K_PLUS,   ___N___,  ___N___,  K_LPRN,   K_LCBR,             K_RCBR,   K_RPRN,   KC_P4,    KC_P5,    KC_P6,    KC_PDOT,
+    _______,  K_MINS,   ___N___,  ___N___,  K_SLSH,   K_LT,               K_GT,     K_BSLS,   KC_P1,    KC_P2,    KC_P3,    KC_P0,
+                                            ___N___,  KC_SPC,   ___N___,  __XXX__,  K_DOT
   ),
 
   [_EXT] = LAYOUT_reviung41(
-    _______,  ___N___,  KC_2,     KC_E,     ___N___,  ___N___,            ___N___,  ___N___,  ___N___,  ___N___,  ___N___,  _______,
+    _______,  ___N___,  G(KC_2),  G(KC_E),  ___N___,  C(KC_T),            ___N___,  ___N___,  ___N___,  G(KC_L),  ___N___,  _______,
     _______,  K_LGUI,   K_LALT,   K_LCTL,   K_LSFT,   ___N___,            ___N___,  ___N___,  KC_PGUP,  KC_HOME,  KC_UP,    KC_END,
     _______,  KC_Z,     KC_X,     KC_C,     KC_V,     ___N___,            ___N___,  ___N___,  KC_PGDN,  KC_LEFT,  KC_DOWN,  KC_RGHT,
                                             KC_LALT,  KC_SPC,   ___N___,  KC_ENT,   __XXX__
@@ -413,7 +413,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     rgblight_set_layer_state(5, layer_state_cmp(state, _SYM));
     rgblight_set_layer_state(6, layer_state_cmp(state, _EXT));
     // Special handling for alt-tab
-    if (isAltReg && !IS_LAYER_ON_STATE(state, _EXT)) {
+    if (isAltReg && !IS_LAYER_ON_STATE(state, _NAV)) {
         unregister_code(KC_LALT);
         isAltReg = false;
     }
@@ -450,9 +450,9 @@ void caps_word_set_user(bool active) {
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case L_BOT:
-            return 100;
-        case NUM_SPC:
-            return 150;
+            return 125;
+        // case NUM_SPC:
+        //     return 150;
         default:
             return TAPPING_TERM;
     }
