b0722b700dac8696093e4497dc0ca06665379572
Stefan Lindholm <stefan.lindholm@sectra.se>
2022-11-01 21:25:03 +0100

    [Reviung] Moving everything and space in the middle!

--- keyboards/reviung/reviung41/keymaps/stonedeaf/keymap.c
+++ keyboards/reviung/reviung41/keymaps/stonedeaf/keymap.c
@@ -72,7 +72,7 @@ enum custom_keycodes {
 #define R_BOT RCTL_T(KC_ESC)
 
 #define FUN_SPC LT(_FUN, KC_SPC)
-#define NUM_SPC LT(_NUM, KC_SPC)
+#define NUM_BSPC LT(_NUM, KC_BSPC)
 #define SYM_ENT LT(_SYM, KC_ENT)
 #define NAV LT(_NAV, KC_LGUI)//MO(_NAV)
 #define EXT MO(_EXT)
@@ -85,7 +85,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,               KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
     L_MID,    KC_A,     KC_S,     KC_D,     KC_F,     KC_G,               KC_H,     KC_J,     KC_K,     KC_L,     MY_SCLN,  R_MID,
     L_BOT,    KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,               KC_N,     KC_M,     K_COMM,   K_DOT,    K_MINS,   R_BOT,
-                                            NAV,      NUM_SPC, TT(_FUN),  SYM_ENT,  EXT
+                                            NAV,      NUM_BSPC, KC_SPC,   SYM_ENT,  TT(_FUN)
   ),
 
   [_SWE] = LAYOUT_reviung41(
@@ -99,35 +99,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,              KC_F6,    KC_F7,    KC_VOLU,  KC_CALC,  KC_PSCR,  _______,
     _______,  KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,             KC_HOME,  ___N___,  KC_VOLD,  ___N___,  ___N___,  KC_RSFT,
     _______,  ___N___,  ___N___,  ___N___,  ___N___,  KC_DEL,             KC_END,   ___N___,  KC_MPLY,  ___N___,  KC_LALT,  KC_RCTL,
-                                            KC_WBAK,  VS_BACK, TT(_FUN),  VS_FWD,  KC_WFWD
+                                            KC_WBAK,  VS_BACK,  KC_SPC,   VS_FWD,  TT(_FUN)
   ),
 
   [_NUM] = LAYOUT_reviung41(
-    KC_0,     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,               KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     SW_AA,
-    K_TILD,   K_EXLM,   K_AT,     K_HASH,   K_DLR,    K_PERC,             ___N___,  KC_4,     KC_5,     KC_6,     SW_OE,    SW_AE,
-    _______,  C(KC_Z),  C(KC_X),  C(KC_C),  C(KC_V),  ___N___,            KC_0,     KC_1,     KC_2,     KC_3,     ___N___,  _______,
-                                            ___N___,  __XXX__,  ___N___,  KC_0,     KC_DOT
+    KC_0,     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,               KC_PGUP,  KC_HOME,  KC_UP,    KC_END,   K_PERC,   SW_AA,
+    _______,  K_LGUI,   K_LALT,   K_LCTL,   K_LSFT,   ___N___,            KC_PGDN,  KC_LEFT,  KC_DOWN,  KC_RGHT,  SW_OE,    SW_AE,
+    _______,  C(KC_Z),  C(KC_X),  C(KC_C),  C(KC_V),  ___N___,            ___N___,  C_LEFT,   ___N___,  C_RGHT,   ___N___,  _______,
+                                            ___N___,  __XXX__,  KC_SPC,   KC_RCTL,  ___N___
   ),
 
   [_NAV] = LAYOUT_reviung41(
-    ALT_TAB,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,               KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     _______,
-    _______,  K_EXLM,   K_AT,     K_HASH,   K_DLR,    K_PERC,             K_TILD,   K_CIRC,   KC_PGUP,  KC_HOME,  KC_UP,    KC_END,
-    _______,  C(KC_Z),  C(KC_X),  C(KC_C),  C(KC_V),  ___N___,            ___N___,  ___N___,  KC_PGDN,  KC_LEFT,  KC_DOWN,  KC_RGHT,
-                                            __XXX__,  KC_SPC,   ___N___,  KC_ENT,   KC_RCTL    
+    ALT_TAB,  G(KC_L),  G(KC_2),  G(KC_E),  ___N___,  C(KC_T),            ___N___,  ___N___,  ___N___,  ___N___,  ___N___,  _______,
+    _______,  ___N___,  ___N___,  ___N___,  ___N___,  ___N___,            ___N___,  K_RSFT,   K_RCTL,   K_LALT,   K_RGUI,   _______,
+    _______,  ___N___,  ___N___,  ___N___,  ___N___,  ___N___,            ___N___,  ___N___,  ___N___,  ___N___,  ___N___,  _______,
+                                            __XXX__,  ___N___,   KC_SPC,  KC_ENT,   KC_RCTL    
   ),
 
   [_SYM] = LAYOUT_reviung41(
-    _______,  K_EQUAL,  ___N___,  ___N___,  K_PIPE,   K_LBRC,             K_RBRC,   K_AMPR,   KC_P7,    KC_P8,    KC_P9,    _______,
-    _______,  K_PLUS,   ___N___,  ___N___,  K_LPRN,   K_LCBR,             K_RCBR,   K_RPRN,   KC_P4,    KC_P5,    KC_P6,    KC_PDOT,
-    _______,  K_MINS,   ___N___,  ___N___,  K_SLSH,   K_LT,               K_GT,     K_BSLS,   KC_P1,    KC_P2,    KC_P3,    KC_P0,
-                                            ___N___,  KC_SPC,   ___N___,  __XXX__,  K_DOT
+    _______,  KC_PSLS,  KC_P7,    KC_P8,    KC_P9,    KC_PMNS,            K_EXLM,   K_AT,     K_HASH,   K_DLR,    K_PERC,   _______,
+    _______,  KC_PAST,  KC_P4,    KC_P5,    KC_P6,    KC_PPLS,            ___N___,  ___N___,  ___N___,  ___N___,  ___N___,  _______,
+    _______,  ___N___,  KC_P1,    KC_P2,    KC_P3,    ___N___,            ___N___,  ___N___,  ___N___,  ___N___,  ___N___,  _______,
+                                            K_DOT,    KC_P0,     KC_SPC,  __XXX__,  K_DOT
   ),
 
   [_EXT] = LAYOUT_reviung41(
-    _______,  ___N___,  G(KC_2),  G(KC_E),  ___N___,  C(KC_T),            ___N___,  ___N___,  ___N___,  G(KC_L),  ___N___,  _______,
+    _______,  ___N___,  ___N___,  ___N___,  ___N___,  C(KC_T),            ___N___,  ___N___,  ___N___,  ___N___,  ___N___,  _______,
     _______,  K_LGUI,   K_LALT,   K_LCTL,   K_LSFT,   ___N___,            ___N___,  ___N___,  KC_PGUP,  KC_HOME,  KC_UP,    KC_END,
-    _______,  KC_Z,     KC_X,     KC_C,     KC_V,     ___N___,            ___N___,  ___N___,  KC_PGDN,  KC_LEFT,  KC_DOWN,  KC_RGHT,
-                                            KC_LALT,  KC_SPC,   ___N___,  KC_ENT,   __XXX__
+    _______,  ___N___,  ___N___,  ___N___,  ___N___,  ___N___,            ___N___,  ___N___,  KC_PGDN,  KC_LEFT,  KC_DOWN,  KC_RGHT,
+                                            KC_LALT,  ___N___,   KC_SPC,  KC_ENT,   __XXX__
   ),
 
   [_COM] = LAYOUT_reviung41(
@@ -457,22 +457,22 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
             return TAPPING_TERM;
     }
 }
-bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case NUM_SPC:
-            return false;
-        default:
-            return true;
-    }
-}
-bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case NUM_SPC:
-            return false;
-        default:
-            return true;
-    }
-}
+// bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
+//     switch (keycode) {
+//         case NUM_SPC:
+//             return false;
+//         default:
+//             return true;
+//     }
+// }
+// bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
+//     switch (keycode) {
+//         case NUM_SPC:
+//             return false;
+//         default:
+//             return true;
+//     }
+// }
 
 bool caps_word_press_user(uint16_t keycode) {
     switch (keycode) {
